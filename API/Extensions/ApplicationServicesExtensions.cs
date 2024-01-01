df5845ca6ebc3af55ab654a59355356357122bc6
premkumar jn <premkumar.jn@frontiersin.net>
2024-01-01 23:40:20 +0530

    Initial Checkin

---
+++ API/Extensions/ApplicationServicesExtensions.cs
@@ -0,0 +1,34 @@
+using Core.Interfaces;
+using Infrastructue.Data;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions
+{
+    public static class ApplicationServicesExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services,
+            IConfiguration config)
+        {
+            services.AddDbContext<StoreContext>(opt =>
+            {
+                opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
+          
+            services.AddScoped<IProductRepository, ProductRepository>();
+           
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy => 
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
+
+            return services;
+        }
+    }
+}
